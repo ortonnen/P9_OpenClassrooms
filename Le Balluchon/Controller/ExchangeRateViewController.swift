40b670511d31186a9bdee6eb5b26f39570f33342
ortonnen <ortonne.n@gmail.com>
2020-07-08 14:48:06 +0200

    file creation

---
+++ Le Balluchon/Controller/ExchangeRateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeRateViewController.swift
+//  Le Balluchon
+//
+//  Created by Nathalie Ortonne on 08/07/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeRateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Le Balluchon/Model/Translation.swift
@@ -0,0 +1,13 @@
+//
+//  Translation.swift
+//  Le Balluchon
+//
+//  Created by Nathalie Ortonne on 08/07/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import Foundation
+
+struct Translation {
+    
+}
