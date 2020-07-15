68e4b59053e83b2ec535320d75fd6cd006e49987
ortonnen <ortonne.n@gmail.com>
2020-07-15 11:15:56 +0200

    add UIPickerView

---
+++ Le Baluchon/Model/Currencies.swift
@@ -0,0 +1,11 @@
+//
+//  currencies.swift
+//  Le Baluchon
+//
+//  Created by Nathalie Ortonne on 15/07/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import Foundation
+
+let currencies = ["Euro","Dollards","Livre","Yen"]
