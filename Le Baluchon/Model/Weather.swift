2776e683b06d28ce908fd8e84797707db283443a
ortonnen <ortonne.n@gmail.com>
2020-07-22 15:40:16 +0200

    preparation of the weather structure

--- Le Baluchon/Model/Weather.swift
+++ Le Baluchon/Model/Weather.swift
@@ -9,67 +9,38 @@
 import Foundation
 
 struct Weather: Decodable {
-    /*
-Localisatopn géographique longitude/ latitude
-     coord:
-        lat:float
-        long:float
-
-info sur les conditions météorologiques
-     weather:
-        id: Int (identifiant des condi météo)
-        main: String (groupe de parametres météo: pluie, neige, extrème..)
-        description: String (conditions météo au sein du groupr 15/fr/Français)
-        icon: String (id icon météo)
-
-parametre interne
-     base: String
-
-     main:
-        temp:Float (temperature mesure par defaut Kelvin/ metrique: celsius)
-        feels_like: Float (temperature/ perception humaine)
-        temp_min: Float (temp minimal en ce moment)
-        temp_max: Float (temp max en ce moment)
-        pressure: Int (pression atmospherique)
-        humidity: Int (% d'humidité)
-
-     visibility: Int
-
-
-     wind:
-        speed: Double (vit du vent unit par défaut m/s)
-        deg: Int (direction du vent)
-
-
-     clouds:
-        all: Int ( % de nébulosité)
-
-heure du calcul de données
-     dt: Float
-
-     sys:
-        type: Int ( parametre interne)
-        id: Int ( parametre interne)
-        country: String ( code du pays )
-        sunrise: Int ( heure du levé de soleil)
-        sunset: Int (heure du coucher du soleil)
-
-décalageen quelques secondes depuis UTC
-     timezone: Int
-
-identifiant de la ville
-     id: Int
-
-nom de la ville
-     name: String
-
-parametre interne
-     cod: Int
-
-     */
-
+    
+    let name: String
+    let weatherCondition: WeatherCondition
+    let temperature : Temperature
+    let wind: Wind
+    let sys : Sys
 }
 
-struct LocalWeather {
-    
+struct WeatherCondition: Decodable {
+
+    var id: Int //(identifiant des condi météo)
+    var main: String //(groupe de parametres météo: pluie, neige, extrème..)
+    var description: String //(conditions météo au sein du groupe)
+    var icon: String //(id icon météo)
+}
+
+struct Temperature: Decodable {
+
+    var temp:Float //(temperature mesure par defaut Kelvin/ metrique: celsius)
+    var humidity: Int //(% d'humidité)
+}
+
+struct Wind: Decodable {
+
+    var speed: Double //(vit du vent unit par défaut m/s)
+    var deg: Int //(direction du vent)
+}
+
+struct Sys: Decodable {
+    var type: Int //( parametre interne)
+    var id: Int //( parametre interne)
+    var country: String //( code du pays )
+    var sunrise: Int //( heure du levé de soleil)
+    var sunset: Int //(heure du coucher du soleil)
 }
