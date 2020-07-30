6a73de2cf838849329a2df2374edb7b8a472dca4
ortonnen <ortonne.n@gmail.com>
2020-07-30 15:16:10 +0200

    add dismissKeyboard and Alerte

--- Le Baluchon/Controller/WeatherViewController.swift
+++ Le Baluchon/Controller/WeatherViewController.swift
@@ -9,37 +9,42 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityTextField: UITextField!
-
+    
     @IBOutlet weak var sunriseHourLabel: UILabel!
     @IBOutlet weak var sunsetHourLabel: UILabel!
     @IBOutlet weak var windSpeedLabel: UILabel!
     @IBOutlet weak var windDirectionLabel: UILabel!
     @IBOutlet weak var humidityLevelLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func tappedValidateButton() {
-
+        guard cityTextField.text != "" && cityTextField.text != nil else {
+            return missingArgumentAlerte()
+        }
+        
         searchWeather()
     }
-
+    
     private func searchWeather(){
         WeatherService.shared.getweather(for: cityTextField.text!) { (success, weather) in
             guard success else {
-            print("error success")
-            return}
-
+                print("error success")
+                return self.connectionAlerte()
+            }
+            
             guard let weather = weather else {
                 print("error weather")
-                return
+                return self.connectionAlerte()
             }
+            
             print ("\(weather.temperature.temp)")
             self.temperatureLabel.text = "\(weather.temperature.temp)"
             self.sunriseHourLabel.text = "\(weather.sys.sunrise)"
@@ -49,5 +54,32 @@ class WeatherViewController: UIViewController {
             self.humidityLevelLabel.text = "\(weather.temperature.humidity)"
         }
     }
-
 }
+
+//MARK: Keyboard
+extension WeatherViewController {
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        cityTextField.resignFirstResponder()
+    }
+}
+
+//MARK: Alerte
+extension WeatherViewController {
+
+    private func missingArgumentAlerte() {
+        let alerte = UIAlertController(title: "Erreur", message: "Entrez une expression correcte", preferredStyle: .alert)
+        let alerteAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alerte.addAction(alerteAction)
+        self.present(alerte,animated: true, completion: nil)
+    }
+
+    private func connectionAlerte() {
+        let alerte = UIAlertController(title: "Erreur", message: "un problème est survenue merci de rééssayer plus tard", preferredStyle: .alert)
+        let alerteAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alerte.addAction(alerteAction)
+        self.present(alerte,animated: true, completion: nil)
+    }
+}
+
+
