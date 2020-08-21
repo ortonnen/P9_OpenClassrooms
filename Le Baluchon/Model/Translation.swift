5dc726445d067b7b29a8827554e72cf211b2064c
ortonnen <ortonne.n@gmail.com>
2020-08-21 14:09:06 +0200

    add coddle for translation

--- Le Baluchon/Model/Translation.swift
+++ Le Baluchon/Model/Translation.swift
@@ -8,9 +8,51 @@
 
 import Foundation
 
-struct Translation: Codable {
+//MARK: Translate
+struct Translate: Codable {
+    let data: Translations
+}
 
-    func translate() {
-        // appel reseau traduire un text ecrit
-    }
+struct Translations: Codable {
+    let translations: [Translated]
+}
+
+struct Translated: Codable {
+    let translatedText: String
+}
+
+//MARK: Detected Langage
+struct DetectLanguage: Codable {
+    let data: DataClass
+}
+
+struct DataClass: Codable {
+    let detections: [[Detection]]
+}
+
+struct Detection: Codable {
+
+    let isReliable: Bool
+    let language: String
+}
+
+
+//MARK: Supported Languages
+struct SupportedLanguages: Codable {
+    let data: Languages
+}
+
+struct Languages: Codable {
+    let languages: [ReferencedLanguage]
+}
+
+struct ReferencedLanguage: Codable{
+    let language: String
+    let name: String
+}
+
+//MARK: Error
+struct TranslationStatusCodeError: Codable {
+    let code : Int
+    let message: String
 }
